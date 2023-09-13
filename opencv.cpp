807752be63862b062a5ea9763fc86e159737f913
Ilya Rybakov <47.2region@gmail.com>
2023-09-13 10:11:52 +0300

    Split question+answers to other images

--- opencv.cpp
+++ opencv.cpp
@@ -4,6 +4,8 @@
 #include <opencv2/highgui.hpp>
 #include <iostream>
 #include <sstream>
+#include <set>
+#include <vector>
 
 using namespace std;
 using namespace cv;
@@ -46,10 +48,18 @@ cv::Vec<unsigned char, 3> secondPreviewCell,cv::Vec<unsigned char, 3> firstNextC
    return (nextPixelsIsGray ||  previewPixelsIsGrey|| nextAndPreview) ;
 } 
 
-Mat& ScanImageAndReduceGresToYellow(Mat& I, const uchar* const table)
+void GenerateAndOutputImage(const cv::Range rows, const cv::Range cols, std::string output_name, Mat& I)
+{
+   output_name += ".jpg";
+   Mat rez = I(rows,cols);
+   cv::imwrite(output_name,rez);
+}
+
+Mat& ScanImageAndReduceGreyToYellow(Mat& I, const uchar* const table)
 {
     CV_Assert(I.depth() == CV_8U);
-
+   std::set<int> line_change;
+   std::vector<int> only_yellow_line;
    Vec3b new_color;
    new_color.val[0] = 0;
    new_color.val[1] = 255;
@@ -70,11 +80,49 @@ Mat& ScanImageAndReduceGresToYellow(Mat& I, const uchar* const table)
             I.at<Vec3b>(i-2,j),I.at<Vec3b>(i+1,j),I.at<Vec3b>(i+2,j)) && pixelIsGrey(pixel))
          {
             I.at<Vec3b>(i,j) = new_color;
+            line_change.insert(i);
          }
 
       }
+
    }
 
+   int last_line = -1;
+   for(int i : line_change)
+   {
+      int counter = 0; 
+      for( int j = 0; j < I.cols;++j)
+      {
+         cv::Vec<unsigned char, 3> pixel = I.at<Vec3b>(i,j);
+         
+         if(pixel[0] == 0 && pixel[1] == 255 && pixel[2] == 255 )
+            ++counter;
+      }
+      if(counter >= I.cols * 0.7)
+      {
+         if(last_line == -1 || (i - last_line) > 5 )
+         {
+            only_yellow_line.push_back(i);
+         }
+
+         last_line = i;
+      }
+   }
+
+   last_line = 0;
+   for (int i = 0; i < only_yellow_line.size();++i)
+   {
+      std::string name = ("Result " + std::to_string(i));
+      cv::Range rows (last_line,only_yellow_line[i]);
+      cv::Range cols (0,I.cols);
+      GenerateAndOutputImage(rows,cols,name,I);
+      last_line = only_yellow_line[i];
+   }
+
+   std::string name = "Result " + std::to_string(only_yellow_line.size());
+   cv::Range rows (last_line,I.rows);
+   cv::Range cols (0,I.cols);
+   GenerateAndOutputImage(rows,cols,name,I);
     return I;
 }
 
@@ -100,11 +148,11 @@ int main( int argc, char* argv[])
     uchar table[256];
     
    cv::Mat clone_i = I.clone();
-   J = ScanImageAndReduceGresToYellow(clone_i, table);
+   J = ScanImageAndReduceGreyToYellow(clone_i, table);
    
    cv::imshow("test",J);
    cv::waitKey(); 
-   cv::imwrite("new_image.jpg",J);
+   
    return 0;
 }
 
